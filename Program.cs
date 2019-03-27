956647f70bbf43e4de833a469d515495a34443e7
Steven Laurino <sjlaurino@gmail.com>
2019-03-27 16:03:51 -0600

    game working

--- Program.cs
+++ Program.cs
@@ -10,99 +10,85 @@ namespace helloworld
     static void Main(string[] args)
     {
       Console.Clear();
-      // Console.Write("What is your name: ");
-      // string name = Console.ReadLine();
-      // Console.WriteLine("Hello " + name);
-      // bool valid = false;
-      // while (!valid)
-      // {
-      //     Console.Write("How old are you: ");
-      //     string strAge = Console.ReadLine();
-      //     int age;
-      //     if (Int32.TryParse(strAge, out age))
-      //     {
-      //         valid = true;
-      //         System.Console.WriteLine($"In 10 years you will be {age + 10}");
-      //     }
-      //     else
-      //     {
-      //         Console.Beep();
-      //         System.Console.WriteLine("Please provide a valid number");
-      //     }
-      // }
 
-      //Given user input of a dice number and sides (1d6, 1d4, 3d8, 1d7)
-
-      //     Console.WriteLine("Welcome to the DiceRoller5000");
-      //     bool rolling = true;
-      //     Random mathRandomTool = new Random();
-      //     while (rolling)
-      //     {
-      //         System.Console.Write("ENTER DICE :");
-      //         string input = Console.ReadLine().ToLower();
-      //         string[] arrInputs = input.Split('d');
-      //         int rolls;
-      //         int sides;
-      //         if (arrInputs.Length < 1 || !Int32.TryParse(arrInputs[0], out rolls) || !Int32.TryParse(arrInputs[1], out sides))
-      //         {
-      //             Console.WriteLine("I DONT WORK WITH BAD DATA!!!");
-      //             continue;
-      //         }
-      //         int[] results = new int[rolls];
-      //         int total = 0;
-      //         for (int i = 0; i < rolls; i++)
-      //         {
-      //             int number = mathRandomTool.Next(1, sides + 1);
-      //             results[i] = number;
-      //             Console.Write(number + " ");
-      //             total += number;
-      //         }
-      //         Console.WriteLine(Environment.NewLine);
-      //         System.Console.WriteLine($"TOTAL: {total}");
-      //     }
-
-      // }
-
-      Console.WriteLine("Welcome to the Rock, Paper, Scissors");
-      bool rolling = true;
+      Console.WriteLine("Welcome to Rock, Paper, Scissors");
+      bool duelling = true;
       Random mathRandomTool = new Random();
-      while (rolling)
+      while (duelling)
       {
-        Console.Write("ENTER ATTACK :");
+        Console.Write("Type rock, paper, or scissors: ");
         string input = Console.ReadLine().ToLower();
+
+        string[] cpu = new string[3];
+        cpu[0] = "rock";
+        cpu[1] = "paper";
+        cpu[2] = "scissors";
+
+        int number = mathRandomTool.Next(0, cpu.Length);
+        string cpuChoice = cpu[number];
 
         if (input == "rock")
         {
-          if (cpu == "rock")
+          if (cpuChoice == "rock")
           {
             Console.WriteLine("DRAW... try again");
           }
-          else if (cpu == "scissors")
+          else if (cpuChoice == "scissors")
           {
             Console.WriteLine("YOU WIN!");
           }
-          else if (cpu == "paper")
+          else if (cpuChoice == "paper")
           {
             Console.WriteLine("YOU LOSE... try again");
           }
+          else
+          {
+            Console.WriteLine("I DONT WORK WITH BAD DATA!!!");
+            continue;
+          }
         }
+        if (input == "scissors")
         {
-          Console.WriteLine("I DONT WORK WITH BAD DATA!!!");
-          continue;
+          if (cpuChoice == "scissors")
+          {
+            Console.WriteLine("DRAW... try again");
+          }
+          else if (cpuChoice == "paper")
+          {
+            Console.WriteLine("YOU WIN!");
+          }
+          else if (cpuChoice == "rock")
+          {
+            Console.WriteLine("YOU LOSE... try again");
+          }
+          else
+          {
+            Console.WriteLine("I DONT WORK WITH BAD DATA!!!");
+            continue;
+          }
         }
-        int[] results = new int[rolls];
-        int total = 0;
-        for (int i = 0; i < rolls; i++)
+        if (input == "paper")
         {
-          int number = mathRandomTool.Next(1, sides + 1);
-          results[i] = number;
-          Console.Write(number + " ");
-          total += number;
-        }
-        Console.WriteLine(Environment.NewLine);
-        Console.WriteLine($"TOTAL: {total}");
-      }
+          if (cpuChoice == "paper")
+          {
+            Console.WriteLine("DRAW... try again");
+          }
+          else if (cpuChoice == "rock")
+          {
+            Console.WriteLine("YOU WIN!");
+          }
+          else if (cpuChoice == "scissors")
+          {
+            Console.WriteLine("YOU LOSE... try again");
+          }
+          else
+          {
+            Console.WriteLine("I DONT WORK WITH BAD DATA!!!");
+            continue;
+          }
 
+        }
+      }
     }
   }
 }
